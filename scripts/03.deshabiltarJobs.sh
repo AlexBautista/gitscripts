e453ed9d3f5ab0d83b8fc41078e40aae2784d6ae
AlexBautista <alex.balat@gmail.com>
2026-03-06 11:11:49 -0600

    Update 03.deshabiltarJobs.sh

--- scripts/03.deshabiltarJobs.sh
+++ scripts/03.deshabiltarJobs.sh
@@ -1,1 +1,20 @@
-echo "************* Ejecutando 03.deshabiltarJobs.sh *************"
+#!/bin/bash
+
+## IMPORTANTE
+## SE REQUIERE EL PARAMETRO DEL REPOSITORIO GIT ON PREMISE y GITHUB PARA LA EJECUCIÓN DE ESTE SHELL, POR EJEMPLO:
+##              A0008-PaymentManagement/PaymentBox
+##              mxapm0001121-a0008-paymentmanagement-paymentbox
+
+## Directorios
+shellsDir=$(pwd)
+cd ..
+baseDir=$(pwd)
+#cd recursos
+resourceDir=$(pwd)
+cd $shellsDir
+echo "==========================================================="
+echo "|                                                         |"
+echo "|   script3333333333333333                                    |"
+echo "|                                                         |"
+echo "==========================================================="
+echo ""
